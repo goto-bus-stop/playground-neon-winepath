3e880b5ca1e2a75556b500ef24a4cf30cf3cc01e
Renée Kooi <renee@kooi.me>
2019-11-19 11:39:40 +0100

    canonicalize path before sending it into to_wine_path

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,6 +1,19 @@
 use neon::prelude::*;
 use winepath::{WinePathError, WineConfig};
 
+trait OrPanic<R> {
+    fn or_panic(self) -> R;
+}
+
+impl<R, E> OrPanic<R> for Result<R, E> where E: std::error::Error {
+    fn or_panic(self) -> R {
+        match self {
+            Ok(r) => r,
+            Err(e) => panic!("{}", e),
+        }
+    }
+}
+
 declare_types! {
     pub class JsWineConfig for WineConfig {
         init(mut cx) {
@@ -9,10 +22,7 @@ declare_types! {
                 return Ok(WineConfig::from_prefix(path.value()));
             }
 
-            match WineConfig::from_env() {
-                Err(err) => panic!("{}", err),
-                Ok(config) => Ok(config),
-            }
+            Ok(WineConfig::from_env().or_panic())
         }
 
         method prefix(mut cx) {
@@ -29,9 +39,10 @@ declare_types! {
         method toWinePath(mut cx) {
             let this = cx.this();
             let input = cx.argument::<JsString>(0)?.value();
+            let input = std::fs::canonicalize(input).or_panic();
             let wine_path = {
                 let guard = cx.lock();
-                let wine_path = this.borrow(&guard).to_wine_path(input).unwrap();
+                let wine_path = this.borrow(&guard).to_wine_path(input).or_panic();
                 wine_path
             };
             Ok(cx.string(wine_path).upcast())
@@ -42,7 +53,7 @@ declare_types! {
             let input = cx.argument::<JsString>(0)?.value();
             let native_path = {
                 let guard = cx.lock();
-                let native_path = this.borrow(&guard).to_native_path(input).unwrap();
+                let native_path = this.borrow(&guard).to_native_path(input).or_panic();
                 native_path.to_string_lossy().to_string()
             };
             Ok(cx.string(native_path).upcast())
